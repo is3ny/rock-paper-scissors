0a9d8228a9466b775c8c11292f8c13110698c2c4
is3ny <supremeep@yandex.ru>
2020-03-24 20:44:09 +0300

    Add glad and glfw. Write basic program to test the correctness.

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <cassert>
 
+#include "glad/glad.h"
+#include "glfw3.h"
+
 #include "Application.hpp"
 
 
 Application* g_application;
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height);
 
 Application::Application()
 {
@@ -39,7 +45,53 @@ int Application::m_Init()
 
 int Application::m_Main()
 {
-    std::cout << "Hello!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    //glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 4);  // Multisampling for smooth resize
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Rock Paper Scissors Automata", 0, 0);
+    if (!window) std::cerr << "Your GPU doesn't support OpenGL 3.3 context! Abort.\n";
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    	puts("Failed to initialize GLAD.");
+    	return -1;
+  	}
+
+    int w, h;
+    glfwGetFramebufferSize(window, &w, &h);
+    glViewport(0, 0, w, h);
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+
+    int frame = 1;
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClearColor(1, 1, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
 
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    std::cout << "Key: " << key << " " << GLFW_KEY_ESCAPE << std::endl;
+    if (key == GLFW_KEY_ESCAPE) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        std::cout << "ESC pressed.\n";
+    }
+}
+
+
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
